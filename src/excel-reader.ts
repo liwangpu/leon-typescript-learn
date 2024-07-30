50ad6b447696c71eaca05eabdd4b5bcca8e0a22a
liwang.pu@going-link.com <liwang.pu@going-link.com>
2024-07-31 02:29:40 +0800

    111

--- src/excel-reader.ts
+++ src/excel-reader.ts
@@ -1,37 +1,100 @@
-import * as Excel from 'exceljs';
 import * as dayjs from 'dayjs';
+import * as Excel from 'exceljs';
 import * as fs from 'fs';
-import * as path from 'path';
 import { isNil } from 'lodash';
+
+export interface IDataMapping {
+  [property: string]: string[];
+}
+
+export interface IDataTypeMapping {
+  [property: string]: DataType;
+}
+
+export enum DataType {
+  number = 'number',
+  string = 'string',
+  date = 'date',
+}
 
 export interface IReadExcelProps {
   filename: string;
-  worksheets: (number | string)[];
+  worksheets?: (number | string)[];
   headerRowIndex: number;
+  dataMapping: IDataMapping;
+  dataTypeMapping?: IDataTypeMapping;
 }
 
 export async function readExcel<T = any>(props: IReadExcelProps): Promise<T[]> {
   const datas: any[] = [];
-  const { filename, worksheets, headerRowIndex } = props;
+  const { filename, worksheets, headerRowIndex, dataMapping, dataTypeMapping } = props;
   console.log(`filePath:`, filename);
-  if (isNil(filename) || !worksheets.length) return datas;
+  if (isNil(filename)) return datas;
   const workbook = new Excel.Workbook();
   if (!fs.existsSync(filename)) {
     throw new Error(`没有在 ${filename} 路径下找到任何文件!`);
   }
   await workbook.xlsx.readFile(filename);
-  for (const ws of worksheets) {
-    const sheet = workbook.getWorksheet(ws);
 
-    console.log(`ws:`, ws);
-    console.log(`sheet:`, sheet);
-    if (sheet) {
-
-      // sheet.eachRow((row, rowIndex) => {
-      //   console.log(`rowIndex:`, rowIndex);
-      // });
+  // 数据属性和表格列标映射
+  const propertyColMap = new Map<number, string>();
+  const headerTitlePropertyMap = new Map<string, string>();
+  (() => {
+    const properties = Object.keys(dataMapping);
+    for (const property of properties) {
+      const titles = dataMapping[property] || [];
+      for (const title of titles) {
+        headerTitlePropertyMap.set(title, property);
+      }
     }
-  }
+  })();
 
+
+
+  workbook.eachSheet((sheet, sheetId) => {
+    sheet.eachRow((row, rowIndex) => {
+      if (rowIndex < headerRowIndex) return;
+      // 标题行
+      if (rowIndex === headerRowIndex) {
+        row.eachCell((cell, colNumber) => {
+          let title = cell.value;
+          if (isNil(title)) return;
+          title = `${title}`.trim();
+          if (!headerTitlePropertyMap.has(title)) return;
+          const property = headerTitlePropertyMap.get(title);
+          propertyColMap.set(colNumber, property);
+        });
+        // console.log(`propertyColMap:`, propertyColMap);
+      } else {
+        if (!propertyColMap.size) return;
+        const data: Record<string, any> = {};
+        // 数据行
+        row.eachCell((cell, colNumber) => {
+          if (!propertyColMap.has(colNumber)) return;
+          const property = propertyColMap.get(colNumber);
+          const valueType = dataTypeMapping[property] || DataType.string;
+          data[property] = dataTransfer({ value: cell.value, type: valueType });
+        });
+        datas.push(data);
+      }
+
+      // row.eachCell();
+
+    });
+  });
   return datas;
+}
+
+function dataTransfer(props: { value: any, type?: DataType }) {
+  const { value, type } = props;
+  switch (type) {
+    case DataType.number:
+      return Number(value);
+    case DataType.date:
+      if (!value) return null;
+      const d = dayjs(value);
+      return d.isValid() ? d.valueOf() : null;
+    default:
+      return value;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,103 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { readExcel } from './excel-reader';
+import { DataType, readExcel } from './excel-reader';
 
 // const filename = path.join(__dirname, '../', 'files', '虾皮本土.xlsx');
-const filename = path.join(__dirname, '../', 'files', 'a.xlsx');
+const filename = path.join(__dirname, '../', 'files', '虾皮本土1.xlsx');
+// const filename = path.join(__dirname, '../', 'files', 'a.xlsx');
+
+
+const dataMapping = {
+  orderNo: ['Order ID'],
+  orderStatus: ['Order Status'],
+  cancelReason: ['Cancel reason'],
+  refundStatus: ['Return / Refund Status'],
+  trackingNumber: ['Tracking Number*'],
+  shippingOption: ['Shipping Option'],
+  shipmentMethod: ['Shipment Method'],
+  estimatedShipOutDate: ['Estimated Ship Out Date'],
+  shipTime: ['Ship Time'],
+  // xxx: ['Order Creation Date'],
+  orderPaidTime: ['Order Paid Time'],
+  sku: ['SKU Reference No.'],
+  variationName: ['Variation Name'],
+  originPrice: ['Original Price'],
+  dealPrice: ['Deal Price'],
+  quanity: ['Quantity'],
+  returnedQuantity: ['Returned quantity'],
+  sellerRebate: ['Seller Rebate'],
+  sellerDiscount: ['Seller Discount'],
+  shopeeRebate: ['Shopee Rebate'],
+  SKUtotalWeight: ['SKU Total Weight'],
+  orderTotalWeight: ['Order Total Weight'],
+  deliveryAddress: ['Delivery Address'],
+  orderCompleteDate: ['Order Complete Time'],
+  // xxx: [''],
+  // xxx: [''],
+  // xxx: [''],
+  // xxx: [''],
+  // xxx: [''],
+};
+
+const dataTypeMapping = {
+  estimatedShipOutDate: DataType.date,
+  shipTime: DataType.date,
+  orderPaidTime: DataType.date,
+  orderCompleteDate: DataType.date,
+  originPrice: DataType.number,
+  dealPrice: DataType.number,
+  quanity: DataType.number,
+  returnedQuantity: DataType.number,
+  sellerRebate: DataType.number,
+  sellerDiscount: DataType.number,
+  shopeeRebate: DataType.number,
+  orderTotalWeight: DataType.number,
+};
+
 
 
 (async () => {
   const datas = await readExcel({
     filename,
-    worksheets: [1],
+    // worksheets: ['orders'],
     headerRowIndex: 1,
+    dataMapping,
+    dataTypeMapping,
   });
   console.log(`datas:`, datas);
 })();
 
+
+// No of product in order
+// Order Total Weight
+// Voucher Code
+// Seller Voucher
+// Seller Absorbed Coin Cashback
+// Shopee Voucher
+// Bundle Deal Indicator
+// Shopee Bundle Discount
+// Seller Bundle Discount
+// Shopee Coins Offset
+// Credit Card Discount Total
+// Total Amount
+// Buyer Paid Shipping Fee
+// Shipping Rebate Estimate
+// Reverse Shipping Fee
+// Transaction Fee
+// Commission Fee
+// Service Fee
+// Grand Total
+// Estimated Shipping Fee
+// Username (Buyer)
+// Receiver Name
+// Phone Number
+// Delivery Address
+// Town
+// District
+// City
+// Province
+// Country
+// Zip Code
+// Remark from buyer
+// Order Complete Time
+// Note
